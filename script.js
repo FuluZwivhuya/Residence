b07bdd0d0b9318476121909144496d0840842343
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2025-03-09 11:27:08 +0200

    initialising project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Admin login buuton
+const loginButton = document.querySelector(".Admin.Login");
+
+// Add a click event listener to the button
+loginButton.addEventListener("click", () => {
+    window.location.href = "login.html"; // Replace with the URL of your login page
+});
